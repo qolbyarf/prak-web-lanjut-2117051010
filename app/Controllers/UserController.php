b30eb4944832b3af51ebe3f9a8cc9d6b955ad120
Qolby Arief <putraendala@gmail.com>
2023-09-29 08:02:44 +0700

    tugas 4

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Models\KelasModel;  
+use App\Models\UserModel;
 use App\Controllers\BaseController;
 
 class UserController extends BaseController
 {
+    protected $helpers=['form'];
     public function index()
     {
         return view('welcome_message');
+        
     }
     public function profile($nama = "", $kelas = "", $npm = "")
     {
@@ -20,14 +24,59 @@ class UserController extends BaseController
         return view('profile', $data);
     }
     public function create() {
-        return view('create_user');
+        $kelas = [
+            [
+                'id' => 1, 
+                'nama_kelas' => 'A'
+            ], 
+            [
+                'id' => 2, 
+                'nama_kelas' => 'B'
+            ], 
+            [
+                'id' => 3, 
+                'nama_kelas' => 'C'
+            ], 
+            [
+                'id' => 4, 
+                'nama_kelas' => 'D'
+            ], 
+        ];
+        $data =[
+            'kelas' => $kelas,
+        ];
+        return view('create_user', $data);
     }
     public function store(){
+        $kelasModel = new KelasModel();
+        if($this->request->getVar('kelas')!= ''){
+            $kelas_select = $kelasModel->where('id', $this->request->getVar('kelas'))->first();
+            $nama_kelas = $kelas_select['nama_kelas'];
+        }else{
+            $nama_kelas ='';
+        }
+
+        $userModel = new UserModel();
+        if (!$this->validate([
+            'nama'  => 'required|is_unique[user.nama]',
+            'npm'   => 'required|is_unique[user.npm]',
+            'kelas' => 'required' 
+        ])){
+            session()->setFlashdata('nama_kelas');
+            return redirect()->back()->withInput()->with('nama_kelas', $nama_kelas);
+        }
+
+        $userModel->saveUser([
+            'nama' => $this->request->getVar('nama'), 
+            'id_kelas' => $this->request->getVar('kelas'), 
+            'npm' => $this->request->getVar('npm'),
+        ]);
         $data=[
             'nama' => $this->request->getVar('nama'),
-            'npm' => $this->request->getVar('npm'),
-            'kelas' => $this->request->getVar('kelas')
+            'kelas' => $this->request->getVar('kelas'),
+            'npm' => $this->request->getVar('npm'), 
         ];
         return view('profile', $data);
     }
+
 }
